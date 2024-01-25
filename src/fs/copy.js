d17f5155511f60d79015f25bb3cde8cdba556805
VukZh <vukfx@inbox.ru>
2024-01-25 22:47:30 +0600

    feat: implement copy, delete, rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
+import {mkdir, copyFile, readdir, access} from "fs/promises";
+import {dirname, join} from "path";
+import {fileURLToPath} from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const currentFilePath = fileURLToPath(import.meta.url)
+    const currentDir = dirname(currentFilePath)
+    const newFilesDir = join(currentDir, "files_copy")
+
+    try {
+        await access(newFilesDir);
+        throw new Error("FS operation failed")
+    } catch (error) {
+
+        if (error.code === "ENOENT") {
+            try {
+                const filesDir = join(currentDir, "files")
+                const filesArray = await readdir(filesDir);
+                await mkdir(newFilesDir)
+
+                for (const file of filesArray) {
+                    const pathFrom = join(currentDir, "files", file)
+                    const pathTo = join(currentDir, "files_copy", file)
+                    await copyFile(pathFrom, pathTo)
+                }
+            } catch (error) {
+                throw new Error("FS operation failed")
+            }
+        } else {
+            throw error
+        }
+    }
+
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import {rename as renameFS, access} from "fs/promises";
+import {dirname, join} from "path";
+import {fileURLToPath} from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const currentFilePath = fileURLToPath(import.meta.url)
+    const currentDir = dirname(currentFilePath)
+    const oldFilePath = join(currentDir, "files", "wrongFilename.txt")
+    const newFilePath = join(currentDir, "files", "properFilename.md")
+
+    try {
+        await access(newFilePath);
+        throw new Error("FS operation failed")
+    } catch (error) {
+
+        if (error.code === "ENOENT") {
+            try {
+                await access(oldFilePath)
+                await renameFS(oldFilePath, newFilePath)
+            } catch (error) {
+                throw new Error("FS operation failed")
+            }
+        } else {
+            throw error
+        }
+    }
+
 };
 
 await rename();
