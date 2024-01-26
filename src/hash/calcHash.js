ca27b17b0e9f54f334e4ab0315dd73b26e043670
VukZh <vukfx@inbox.ru>
2024-01-26 19:45:01 +0600

    feat: implement zlib, esm module, hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import {dirname, join} from "path";
+import {fileURLToPath} from 'url';
+import {pipeline} from "stream"
+import {createHash} from "crypto"
+import {createReadStream} from "fs"
+
 const calculateHash = async () => {
-    // Write your code here 
+    const currentFilePath = fileURLToPath(import.meta.url)
+    const currentDir = dirname(currentFilePath)
+    const encodedFile = join(currentDir, "files", "fileToCalculateHashFor.txt")
+
+    const readStream = createReadStream(encodedFile)
+    const encodedStream = createHash("sha256")
+    encodedStream.setEncoding('hex')
+    const writeStream = process.stdout
+
+    try {
+        await pipeline(readStream, encodedStream, writeStream, (err) => {
+            if (err) throw new Error("calculateHash error")
+        })
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 await calculateHash();
