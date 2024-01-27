bee9578bcfa4f6a45c678e36127696d32091fb24
VukZh <vukfx@inbox.ru>
2024-01-27 23:35:13 +0600

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import {Worker} from "worker_threads"
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import {cpus} from "os"
+
 const performCalculations = async () => {
-    // Write your code here
+    const currentFilePath = fileURLToPath(import.meta.url)
+    const currentDir = dirname(currentFilePath)
+    const workerFile = join(currentDir, "worker.js")
+
+    const cpusNumber = cpus().length
+
+    const res = new Array(cpusNumber)
+
+    const getAllResults = () => res.filter(r => r.status).length === cpusNumber
+
+    for (let i = 0; i < cpusNumber; i++) {
+        const worker = new Worker(workerFile, {
+            workerData: 10 + i
+        })
+        worker.on("message", (fiboResult) => {
+            res[i] = {
+                status: "resolved",
+                data: fiboResult
+            }
+            if (getAllResults()) {
+                console.log(res)
+            }
+        })
+        worker.on("error", (msg) => {
+            res[i] = {
+                status: "error",
+                data: null
+            }
+            if (getAllResults()) {
+                console.log(res)
+            }
+        })
+    }
 };
 
 await performCalculations();
