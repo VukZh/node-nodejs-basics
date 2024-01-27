54623734e2db081cac3c83c819d3a024cfe1ea1a
VukZh <vukfx@inbox.ru>
2024-01-27 18:56:24 +0600

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import {spawn} from "child_process"
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const currentFilePath = fileURLToPath(import.meta.url)
+    const currentDir = dirname(currentFilePath)
+    const childFile = join(currentDir, "files", "script.js")
+    const spawned = spawn('node', [childFile, ...args])
+    process.stdin.pipe(spawned.stdin);
+    spawned.stdout.pipe(process.stdout);
+    spawned.on('exit', code => {
+        console.log("child has just closed with code", code)
+    })
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([11, '2s2', [1, 2, true]]);
